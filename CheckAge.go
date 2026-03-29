fbb48bd5e157c669f0b957b8d512cb46eecad83f
unknown <skeroff1@gmail.com>
2026-03-29 13:41:10 +0300

    мини программа на проверку возраста

---
+++ CheckAge.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func CheckAge(age int, name string) {
+	if age >= 18 {
+		fmt.Printf("Вы совершеннолетний, добро пожаловать в наш Бар Рюмка, %s", name)
+	} else if age < 18 {
+		fmt.Println("Вы несовершеннолетний, вход невозможен!")
+	}
+}
+
+func main() {
+	var name string
+	var age int
+	fmt.Print("Введите имя: ")
+	fmt.Scanln(&name)
+	fmt.Print("Введите возраст: ")
+	fmt.Scanln(&age)
+	CheckAge(age, name)
+}
